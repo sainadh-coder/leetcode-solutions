d7e6c01ecbfa7b1f295c14105337e6ef66ac8bee
Sainadh Hari <harisainadh1234@gmail.com>
2026-07-11 11:53:22 +0530

    Time: 0 ms (100%), Space: 46.9 MB (7.67%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,43 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index);
+ *     public int length();
+ * }
+ */
+
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int start = 0;
+        int end = mountainArr.length() - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) > mountainArr.get(mid + 1)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        int peak = start; 
+        int left = 0;
+        int right = peak;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            if (val < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        left = peak + 1;
+        right = mountainArr.length() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            if (val > target) left = mid + 1;   
+            else right = mid - 1;
+        }
+        return -1; 
+    }
+}
