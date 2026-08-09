06221517d341d2b931bf2ef89a5612b59108dae1
Sainadh Hari <harisainadh1234@gmail.com>
2026-08-10 00:41:25 +0530

    Time: 65 ms (24.44%), Space: 19.3 MB (16.67%) - LeetHub

--- 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.py
+++ 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.py
@@ -4,16 +4,17 @@ class Solution:
         for num in nums:
             temp = num
             maxi = 0
-            digits = 0
             while temp > 0:
                 rem = temp % 10
                 if rem > maxi:
                     maxi = rem
-                digits += 1
                 temp //= 10
+            temp = num
+            place = 1
             encrypted = 0
-            while digits > 0:
-                encrypted = encrypted * 10 + maxi
-                digits -= 1
+            while temp > 0:
+                encrypted += maxi * place
+                place *= 10
+                temp //= 10
             ans += encrypted
         return ans
