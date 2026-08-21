baae7bdc44755c5adcc1b0f2b9d1c0be85ff28a4
Sainadh Hari <harisainadh1234@gmail.com>
2026-08-21 20:37:48 +0530

    Time: 1627 ms (8.54%), Space: 19.3 MB (93.9%) - LeetHub

---
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+        def count(x):
+            ans = 0
+            m = len(coins)
+            for mask in range(1, 1 << m):
+                l = 1
+                bits = 0
+
+                for i in range(m):
+                    if mask & (1 << i):
+                        bits += 1
+                        l = l * coins[i] // math.gcd(l, coins[i])
+                        if l > x:
+                            break
+                else:
+                    if bits % 2:
+                        ans += x // l
+                    else:
+                        ans -= x // l
+            return ans
+        low = 1
+        high = min(coins) * k
+        while low < high:
+            mid = (low + high) // 2
+
+            if count(mid) >= k:
+                high = mid
+            else:
+                low = mid + 1
+        return low
